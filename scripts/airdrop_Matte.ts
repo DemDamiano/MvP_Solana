6129ca0a968d506d0ae67f07c98fdfb0edba2018
Damiano <damiano.rovella@fgainformatica.com>
2024-06-28 11:47:01 +0200

    modificato per farlo funzionare sul pc di damiano, file intonso _Matte

--- scripts/airdrop_Matte.ts
+++ scripts/airdrop_Matte.ts
@@ -1,17 +1,12 @@
 import "../config";
 import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
-import * as fs from 'fs';
 
-const privateKey = JSON.parse(fs.readFileSync('./machine.json', 'utf8'));
+import privateKey from "./machine.json";
 
 (async function () {
     const keypair = Keypair.fromSecretKey(new Uint8Array(privateKey));
     console.log("Airdrop to", keypair.publicKey.toBase58());
-
-    // Ensure CLUSTER_URL is defined or provide a default
-    //const clusterUrl = process.env.CLUSTER_URL || 'https://api.mainnet-beta.solana.com';
-    const connection = new Connection("https://api.devnet.solana.com", "finalized");
-
+    const connection = new Connection(process.env.CLUSTER_URL, "finalized");
     console.log("Current balance:", await connection.getBalance(keypair.publicKey));
     const airdropSignature = await connection.requestAirdrop(
         keypair.publicKey,
