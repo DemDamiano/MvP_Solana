9088fc6860f995c598c8083093976fe02a40977d
Matteo Rosani <matteo.rosani@gmail.com>
2024-07-05 10:24:45 +0200

    Bug fixing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import Head from 'next/head';
-import './css/home/style.css';
+import './common.css';
 
-const Layout = ({ children }) => {
+const Layout = ({ 
+  children 
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
     return (
       <html lang="en">
-        <Head>
+        <head>
           <title>Car Reservation</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
+        </head>
         <body>
           <div className="container">
             <aside className="sidebar">
-              <div className="sidebar-icon"><img src="/IMG/home/home-icon.svg" alt="Home" /></div>
-              <div className="sidebar-icon"><img src="/IMG/home/user-icon.svg" alt="User" /></div>
-              <div className="sidebar-icon"><img src="/IMG/home/notification-icon.svg" alt="Notifications" /></div>
-              <div className="sidebar-icon"><img src="/IMG/home/settings-icon.svg" alt="Settings" /></div>
-              <div className="sidebar-icon"><img src="/IMG/home/logout-icon.svg" alt="Logout" /></div>
+              <div className="sidebar-icon"><img src="/IMG/Main/Home/icon-home.png" alt="Home" /></div>
+              <div className="sidebar-icon"><img src="/IMG/Main/Home/icon-user.png" alt="User" /></div>
+              <div className="sidebar-icon"><img src="/IMG/Main/Home/icon-notification.png" alt="Notifications" /></div>
+              <div className="sidebar-icon"><img src="/IMG/Main/Home/icon-settings.png" alt="Settings" /></div>
+              <div className="sidebar-icon"><img src="/IMG/Main/Home/icon-logout.png" alt="Logout" /></div>
             </aside>
             <main className="main-content">
               {children}
